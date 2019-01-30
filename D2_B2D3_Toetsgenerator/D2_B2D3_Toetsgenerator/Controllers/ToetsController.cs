1e9aaaebb297add58e7a916c3b572e57a152a086
JanB99 <46781046+JanB99@users.noreply.github.com>
2019-01-30 12:02:25 +0100

    Add files via upload
    Rick, je moet bij de aangepaste controllers even bovenaan 2 using statements gebruiken. deze statements zijn afgeleid uit het toevoegen van 2 references aan het project. je moet namelijk zowel de Microsoft Word Object library als de Microsoft Excel Object Library toevoegen. Dit doe je door rechtermuisklik op het project in de file explorer. vervolgens naar Add te gaan en dan op References te klikken. Hierna klik je op COM in het menu links, en dan zoek je rechtsboven bij de zoekbalk de boven genoemde libraries op (namelijk Microsoft Word Object library en Microsoft Excel Object Library). en dan voeg je hem toe.

--- D2_B2D3_Toetsgenerator/D2_B2D3_Toetsgenerator/Controllers/ToetsController.cs
+++ D2_B2D3_Toetsgenerator/D2_B2D3_Toetsgenerator/Controllers/ToetsController.cs
@@ -6,19 +6,156 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using D2_B2D3_Toetsgenerator.Models;
+using D1_2_B2D3_Casus_Toetsgenerator.Models;
+using Microsoft.Office.Interop.Word;
+using System.Reflection;
 
-namespace D2_B2D3_Toetsgenerator.Controllers
+namespace D1_2_B2D3_Casus_Toetsgenerator.Controllers
 {
     public class ToetsController : Controller
     {
-        private ToetsgeneratorModel db = new ToetsgeneratorModel();
+        private DBContext db = new DBContext();
 
         // GET: Toets
         public ActionResult Index()
         {
             var toets = db.Toets.Include(t => t.Toetsmatrijs);
             return View(toets.ToList());
+        }
+
+        [AcceptVerbs(HttpVerbs.Get | HttpVerbs.Post)]
+        public ActionResult Convert(int? id, bool isAntwoord)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Toets toets = db.Toets.Find(id);
+            if (toets == null)
+            {
+                return HttpNotFound();
+            }
+
+            //Starten nieuwe Word applicatie.
+            Application app = new Application();
+            object missing = Missing.Value;
+            //Aanmaken nieuw document
+            Document doc = app.Documents.Add(ref missing, ref missing, ref missing, ref missing);
+            app.Visible = true;
+
+            //Paragraaf aanmaken met een tabel die 15 rijen heeft en 2 kolommen.
+            object EndOfDoc = "\\endofdoc";
+            Range rn = doc.Bookmarks.get_Item(ref EndOfDoc).Range;
+            Paragraph para = doc.Paragraphs.Add(rn);
+            Table tb = para.Range.Tables.Add(rn, 15, 2);
+
+            tb.Borders.InsideLineStyle = WdLineStyle.wdLineStyleSingle;
+            tb.Borders.OutsideLineStyle = WdLineStyle.wdLineStyleSingle;
+
+            //Tabel invullen met benodigde informatie
+            tb.Cell(1, 1).Range.Text = "Modulenaam";
+            tb.Cell(1, 2).Range.Text = toets.Toetsmatrijs.moduleNaam;
+            tb.Cell(2, 1).Range.Text = "Modulecode";
+            tb.Cell(2, 2).Range.Text = toets.Toetsmatrijs.moduleCode;
+            tb.Cell(3, 1).Range.Text = "Prestatieindicator";
+            tb.Cell(3, 2).Range.Text = toets.Toetsmatrijs.prestatieIndicator.ToString();
+            tb.Cell(4, 1).Range.Text = "Studiejaar";
+            tb.Cell(4, 2).Range.Text = toets.Toetsmatrijs.aanmaakdatum.Value.Date.Year.ToString();
+            tb.Cell(5, 1).Range.Text = "Soort toets";
+            tb.Cell(5, 2).Range.Text = toets.categorie;
+            tb.Cell(6, 1).Range.Text = "Toetsgelegenheid";
+            tb.Cell(6, 2).Range.Text = "gelegenheid " + toets.toetsgelegenheid;
+            tb.Cell(7, 1).Range.Text = "Tijdsduur";
+            tb.Cell(7, 2).Range.Text = toets.tijdsduur.ToString();
+            tb.Cell(8, 1).Range.Text = "Schrapformulier";
+            tb.Cell(8, 2).Range.Text = toets.schrapformulier.ToString();
+            tb.Cell(9, 1).Range.Text = "Examinatoren";
+            tb.Cell(9, 2).Range.Text = toets.examinatoren;
+
+            int total = 0;
+            foreach (var item in toets.ToetsOpgave)
+            {
+                total += item.Opgave.score;
+            }
+
+            tb.Cell(10, 1).Range.Text = "Te behalen punten (T)";
+            tb.Cell(10, 2).Range.Text = total.ToString();
+
+
+            int gok = 0;
+            foreach (var item in toets.ToetsOpgave)
+            {
+                if (item.Opgave.categorie == "gesloten vraag")
+                {
+                    gok += item.Opgave.score / 2;
+                }
+            }
+
+            tb.Cell(11, 1).Range.Text = "Te gokken punten (G)";
+            tb.Cell(11, 2).Range.Text = gok.ToString();
+            tb.Cell(12, 1).Range.Text = "Punten voor voldoende(Cesuur)";
+            tb.Cell(12, 2).Range.Text = (0.55 * (total - gok) + gok).ToString();
+            tb.Cell(13, 1).Range.Text = "Behaalde punten (P)";
+            tb.Cell(14, 1).Range.Text = "Studentnaam";
+            tb.Cell(15, 1).Range.Text = "Studentnummer";
+
+            
+            para.Range.Text += @"Berekening PI-cijfer:  (P - G) / (T - G) * 10.
+
+De examencommissie kan hierop na toetsanalyse een generieke correctie aanbrengen. 
+Instructies
+- Toegestane hulpmiddelen: schrijfgerei pen met zwarte of blauwe inkt
+- Aan het einde van dit formulier is ruimte die als kladpapier gebruikt kan worden(niet losscheuren!)
+- Controleer de hele set op volledigheid en schakel de surveillant in indien onderdelen ontbreken
+- Maak de opgaven in de aangegeven ruimten op dit formulier
+- Lever na afloop alles in bij de surveillant
+
+
+
+
+
+";
+
+            //Expliciet tekst niet bold maken  
+            para.Range.Bold = 0;
+
+            //Variabelen definieren 
+            string opgave = "";
+            int count = 1;
+            string underscore = "_";
+
+            //per vraag nvullen in Word document
+            foreach (var item in toets.ToetsOpgave)
+            {
+                opgave += count.ToString() + ". " + item.Opgave.inhoud + " ; " + item.Opgave.score + " punten te behalen.";
+                opgave += underscore;
+
+                //Vraag bold maken
+                para.Range.Text += opgave;
+                object start = para.Range.Start;
+                object end = para.Range.Start + opgave.IndexOf("_");
+                Range rnBold = doc.Range(ref start, ref end);
+                rnBold.Bold = 1;
+
+                //per vraag lege tabel toevoegen
+                Range tbRange = doc.Range(ref end, ref missing);
+                Table tbOpgave = para.Range.Tables.Add(tbRange, 1, 1);
+                tbOpgave.Borders.InsideLineStyle = WdLineStyle.wdLineStyleSingle;
+                tbOpgave.Borders.OutsideLineStyle = WdLineStyle.wdLineStyleSingle;
+
+                if (isAntwoord)
+                {
+                    tbOpgave.Cell(1, 1).Range.Text = item.Opgave.antwoorden;
+                    tbOpgave.Cell(1, 1).Range.Font.Color = WdColor.wdColorRed;
+                }
+
+                para.Range.InsertParagraphAfter();
+
+                opgave = "";
+                count++;
+            }
+
+            return RedirectToAction("Index");
         }
 
         // GET: Toets/Details/5
@@ -48,7 +185,7 @@ namespace D2_B2D3_Toetsgenerator.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,matrijsID,categorie,studiejaar,blokperiode,toetsgelegenheid,tijdsduur,schrapformulier,examinatoren,makerID,aanmaakDatum,laatstGewijzigdDoor,datumGewijzigd,status")] Toets toets)
+        public ActionResult Create([Bind(Include = "ID,matrijsID,categorie,studiejaar,blokperiode,toetsgelegenheid,tijdsduur,schrapformulier,examinatoren,maker,aanmaakDatum,laatstGewijzigdDoor,datumGewijzigd,status")] Toets toets)
         {
             if (ModelState.IsValid)
             {
@@ -82,7 +219,7 @@ namespace D2_B2D3_Toetsgenerator.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,matrijsID,categorie,studiejaar,blokperiode,toetsgelegenheid,tijdsduur,schrapformulier,examinatoren,makerID,aanmaakDatum,laatstGewijzigdDoor,datumGewijzigd,status")] Toets toets)
+        public ActionResult Edit([Bind(Include = "ID,matrijsID,categorie,studiejaar,blokperiode,toetsgelegenheid,tijdsduur,schrapformulier,examinatoren,maker,aanmaakDatum,laatstGewijzigdDoor,datumGewijzigd,status")] Toets toets)
         {
             if (ModelState.IsValid)
             {
@@ -127,6 +264,75 @@ namespace D2_B2D3_Toetsgenerator.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        public void WriteWord(bool isAntwoordblad, Document doc, Toets toets) {
+            doc.Content.Font.Size = 11;
+
+            doc.Content.Text += "Modulenaam: " + toets.Toetsmatrijs.moduleNaam;
+            doc.Content.Text += "Modulecode: " + toets.Toetsmatrijs.moduleCode;
+            doc.Content.Text += "Prestatieindicator: " + toets.Toetsmatrijs.prestatieIndicator.ToString();
+            doc.Content.Text += "Studiejaar: " + toets.Toetsmatrijs.aanmaakdatum.Value.Date.Year;
+            doc.Content.Text += "Soort toets: " + toets.categorie;
+            doc.Content.Text += "Toetsgelegenheid: " + "gelegenheid " + toets.toetsgelegenheid;
+            doc.Content.Text += "Tijdsduur: " + toets.tijdsduur.ToString();
+            doc.Content.Text += "Schrapformulier: " + toets.schrapformulier.ToString();
+            doc.Content.Text += "Examinatoren: " + toets.examinatoren;
+
+            int total = 0;
+            foreach (var item in toets.ToetsOpgave)
+            {
+                total += item.Opgave.score;
+            }
+
+            doc.Content.Text += "Te behalen punten: " + total.ToString();
+
+            int gok = 0;
+            foreach (var item in toets.ToetsOpgave)
+            {
+                if (item.Opgave.categorie == "gesloten vraag")
+                {
+                    gok += item.Opgave.score / 2;
+                }
+            }
+
+            doc.Content.Text += "Te gokken punten: " + gok.ToString();
+            doc.Content.Text += "Punten voor voldoende(Cesuur): " + (0.55 * (total - gok) + gok);
+            doc.Content.Text += @"Behaalde punten (P): 
+Studentnaam: 
+Studentnummer:
+
+Berekening PI-cijfer:  (P-G)/(T-G)*10.
+
+De examencommissie kan hierop na toetsanalyse een generieke correctie aanbrengen. 
+Instructies 
+- Toegestane hulpmiddelen: schrijfgerei pen met zwarte of blauwe inkt 
+- Aan het einde van dit formulier is ruimte die als kladpapier gebruikt kan worden(niet losscheuren!) 
+- Controleer de hele set op volledigheid en schakel de surveillant in indien onderdelen ontbreken 
+- Maak de opgaven in de aangegeven ruimten op dit formulier 
+- Lever na afloop alles in bij de surveillant
+
+
+
+
+";
+            int count = 1;
+            string underscores = "";
+            foreach (var item in toets.ToetsOpgave)
+            {
+                doc.Content.Text += count.ToString() + " " + item.Opgave.inhoud;
+                for (int i = 0; i < 164; i++)
+                {
+                    underscores += "_";
+                }
+                doc.Content.Text += underscores;
+                if (isAntwoordblad) {
+
+                    doc.Content.Text += item.Opgave.antwoorden;
+                }
+                underscores = "";
+                count++;
+            }
         }
     }
 }
--- D2_B2D3_Toetsgenerator/D2_B2D3_Toetsgenerator/Controllers/ToetsmatrijsController.cs
+++ D2_B2D3_Toetsgenerator/D2_B2D3_Toetsgenerator/Controllers/ToetsmatrijsController.cs
@@ -6,18 +6,213 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using D2_B2D3_Toetsgenerator.Models;
+using D1_2_B2D3_Casus_Toetsgenerator.Models;
+using Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
-namespace D2_B2D3_Toetsgenerator.Controllers
+namespace D1_2_B2D3_Casus_Toetsgenerator.Controllers
 {
     public class ToetsmatrijsController : Controller
     {
-        private ToetsgeneratorModel db = new ToetsgeneratorModel();
+        private DBContext db = new DBContext();
 
         // GET: Toetsmatrijs
         public ActionResult Index()
         {
             return View(db.Toetsmatrijs.ToList());
+        }
+
+        [AcceptVerbs(HttpVerbs.Get | HttpVerbs.Post)]
+        public ActionResult Convert(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Toetsmatrijs toetsmatrijs = db.Toetsmatrijs.Find(id);
+            if (toetsmatrijs == null)
+            {
+                return HttpNotFound();
+            }
+
+            Microsoft.Office.Interop.Excel.Application oXL;
+            Microsoft.Office.Interop.Excel._Workbook oWB;
+            _Worksheet oSheet;
+
+            //Start Excel and get Application object.
+            oXL = new Application();
+            oXL.Visible = true;
+
+            //verkrijgen nieuw werkboek.
+            oWB = (oXL.Workbooks.Add(Missing.Value));
+            oSheet = oWB.ActiveSheet;
+
+            //Vul toetsmatrijs met gegevens uit de database
+            int row = 9;
+            foreach (var kenniselement in toetsmatrijs.Kenniselement)
+            {
+                oSheet.Cells[row, 1] = kenniselement.inhoud.ToString();
+
+                for (int col = 2; col < 5; col++)
+                {
+                    if (col == 2)
+                    {
+                        oSheet.Cells[row, col] = kenniselement.aantalReproductie.ToString();
+                    }
+                    else if (col == 3)
+                    {
+                        oSheet.Cells[row, col] = kenniselement.aantalBegrip.ToString();
+                    }
+                    else if (col == 4)
+                    {
+                        oSheet.Cells[row, col] = kenniselement.aantalToepassing.ToString();
+                    }
+
+                }
+                row += 1;
+            }
+
+            //Vul toetsmatrijs met gegevens uit de database
+            row = 23;
+            foreach (var kenniselement in toetsmatrijs.Kenniselement)
+            {
+                oSheet.Cells[row, 1] = kenniselement.inhoud.ToString();
+
+                for (int col = 2; col < 5; col++)
+                {
+                    string datastr = "";
+                    if (col == 2)
+                    {
+                        foreach (var opgave in kenniselement.Opgave)
+                        {
+                            if (opgave.typeScore == "Reproductie")
+                            {
+                                if (datastr.Length < 1)
+                                {
+                                    datastr = opgave.ID.ToString();
+                                }
+                                else
+                                {
+                                    datastr = datastr + ", " + opgave.ID.ToString();
+                                }
+                            }
+                        }
+                        oSheet.Cells[row, col] = datastr;
+                    }
+                    else if (col == 3)
+                    {
+                        foreach (var opgave in kenniselement.Opgave)
+                        {
+                            if (opgave.typeScore == "Begrip")
+                            {
+                                if (datastr.Length < 1)
+                                {
+                                    datastr = opgave.ID.ToString();
+                                }
+                                else
+                                {
+                                    datastr = datastr + ", " + opgave.ID.ToString();
+                                }
+                            }
+                        }
+                        oSheet.Cells[row, col] = datastr;
+                    }
+                    else if (col == 4)
+                    {
+                        foreach (var opgave in kenniselement.Opgave)
+                        {
+                            if (opgave.typeScore == "Toepassing")
+                            {
+                                if (datastr.Length < 1)
+                                {
+                                    datastr = opgave.ID.ToString();
+                                }
+                                else
+                                {
+                                    datastr = datastr + ", " + opgave.ID.ToString();
+                                }
+                            }
+                        }
+                        oSheet.Cells[row, col] = datastr;
+                    }
+
+                }
+                row += 1;
+            }
+
+            //Standaard cell gegevens volgens toetsmatrijs
+            oSheet.Cells[1, 1] = "Toetsniveau";
+            oSheet.Cells[1, 2] = "Reproductie";
+            oSheet.Cells[1, 3] = "Begrip";
+            oSheet.Cells[1, 4] = "Toepassing";
+            oSheet.Cells[2, 1] = "Vraagtype";
+            oSheet.Cells[2, 2] = "Kennisvraag";
+            oSheet.Cells[2, 3] = "Inzichtvraag";
+            oSheet.Cells[2, 4] = "Toepassingsvraag";
+            oSheet.Cells[3, 1] = "Streefverdeling voor PI 1";
+            oSheet.Cells[3, 2] = "60%";
+            oSheet.Cells[3, 3] = "40%";
+            oSheet.Cells[3, 4] = "0%";
+            oSheet.Cells[4, 1] = "Streefverdeling voor PI 2";
+            oSheet.Cells[4, 2] = "40%";
+            oSheet.Cells[4, 3] = "60%";
+            oSheet.Cells[4, 4] = "0%";
+            oSheet.Cells[5, 1] = "Streefverdeling voor PI 3";
+            oSheet.Cells[5, 2] = "20%";
+            oSheet.Cells[5, 3] = "30%";
+            oSheet.Cells[5, 4] = "50%";
+            oSheet.Cells[7, 1] = toetsmatrijs.moduleCode + toetsmatrijs.prestatieIndicator.ToString();
+            oSheet.Cells[7, 2] = "Aantal punten";
+            oSheet.Cells[8, 1] = "Kenniselementen";
+            oSheet.Cells[8, 2] = "Reproductie";
+            oSheet.Cells[8, 3] = "Begrip";
+            oSheet.Cells[8, 4] = "Toepassing";
+            oSheet.Cells[8, 5] = "Totaal";
+            oSheet.Cells[8, 6] = "Percentage";
+            oSheet.Cells[17, 1] = "Totaal";
+            oSheet.Cells[18, 1] = "Percentage";
+            oSheet.Cells[21, 1] = "Toetsperiode";
+            oSheet.Cells[21, 2] = "Vraagnummers";
+            oSheet.Cells[22, 1] = "Kenniselementen";
+            oSheet.Cells[22, 2] = "Reproductie";
+            oSheet.Cells[22, 3] = "Begrip";
+            oSheet.Cells[22, 4] = "Toepassing";
+
+            //Standaard style van toetsmatrijs
+            oSheet.Columns.ColumnWidth = 25;
+            oSheet.get_Range("B1", "F30").Columns.ColumnWidth = 14;
+            oSheet.get_Range("A1", "A5").Font.Bold = true;
+            oSheet.get_Range("A7", "D7").Font.Bold = true;
+            oSheet.get_Range("A7", "F18").Borders.LineStyle = XlLineStyle.xlContinuous;
+            oSheet.get_Range("A21", "D30").Borders.LineStyle = XlLineStyle.xlContinuous;
+            oSheet.get_Range("A8", "F8").Font.Bold = true;
+            oSheet.get_Range("A17", "A18").Font.Bold = true;
+            oSheet.get_Range("A21", "B21").Font.Bold = true;
+            oSheet.get_Range("A21", "A22").Font.Bold = true;
+            oSheet.get_Range("A22", "D22").Font.Bold = true;
+            oSheet.get_Range("B7", "D7").MergeCells = true;
+            oSheet.get_Range("B7", "D7").HorizontalAlignment = XlHAlign.xlHAlignCenter;
+            oSheet.get_Range("B21", "D21").MergeCells = true;
+            oSheet.get_Range("B21", "D21").HorizontalAlignment = XlHAlign.xlHAlignCenter;
+            oSheet.get_Range("A1", "D5").Interior.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.FromArgb(255, 255, 204));
+            oSheet.get_Range("B9", "D16").Interior.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.FromArgb(255, 204, 153));
+            oSheet.get_Range("F9", "F17").NumberFormat = "0%";
+            oSheet.get_Range("B18", "E18").NumberFormat = "0%";
+
+            //Standaard formules toetsmatrijs
+            oSheet.get_Range("F9", "F16").Formula = "=E9 / E$17";
+            oSheet.get_Range("E9", "E16").Formula = "=SUM(B9:D9)";
+            oSheet.Cells[17, 5].Formula = "=SUM(E9:E16)";
+            oSheet.Cells[17, 6].Formula = "=E17 / E$17";
+            oSheet.Cells[18, 5].Formula = "=E17 / E$17 ";
+            oSheet.Cells[17, 2].Formula = "=SUM(B9:B16)";
+            oSheet.Cells[17, 3].Formula = "=SUM(C9:C16)";
+            oSheet.Cells[17, 4].Formula = "=SUM(D9:D16)";
+            oSheet.get_Range("B18", "D18").Formula = "=B17 / $E17";
+
+
+
+            return RedirectToAction("Index");
         }
 
         // GET: Toetsmatrijs/Details/5
